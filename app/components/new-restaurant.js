853b6fe6f086b8b94ecd38c4b0698fa4b524c230
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-21 12:18:00 +0300

    create a form to create restaurant at admin route

--- app/components/new-restaurant.js
+++ app/components/new-restaurant.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	isCreateFormVisible:false,
+	actions:{
+		showCreateForm(){
+			this.set('isCreateFormVisible', true)
+		},
+		cancel(){
+			this.set('isCreateFormVisible', false)
+		},
+		create(){
+			var params = {
+				name: this.get('name'),
+				description: this.get('description'),
+				image: this.get('image'),
+				longitude: parseInt(this.get('longitude')),
+				latitude: parseInt(this.get('latitude')),
+				type: this.get('type'),
+				star_rating: parseInt(this.get('star_rating'))
+			};
+			this.sendAction('createRestaurant', params);
+			this.setProperties({'isCreateFormVisible':false,'image':'','name':'','description':'','longitude':'','latitude':'','type':'','star_rating':''});
+		}
+	}
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model(){
-		return this.store.findAll('restaurant');
+	actions:{
+		createRestaurant(params){
+			var newRestaurant = this.store.createRecord('restaurant', params);
+			newRestaurant.save();
+			this.transitionTo('admin');
+		}
 	}
 });

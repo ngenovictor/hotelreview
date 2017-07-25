0b74e1890b8a040a6a7d1a3d63a135424b50091a
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-25 07:40:03 +0300

    update app

--- app/helpers/if-in.js
+++ app/helpers/if-in.js
@@ -4,9 +4,9 @@ export function ifIn(params) {
 	var restaurant = params[0];
 	var myList = params[1].favRestaurants;
 	if(myList.includes(restaurant)){
-		return '<i class="fa fa-star fa-2x" {{action "removeRestaurant" restaurant}}></i>'
+		return Ember.String.htmlSafe('<i class="fa fa-star fa-2x" {{action "removeRestaurant" restaurant}}></i>');
 	}else{
-		return '<i class="fa fa-star-0 fa-2x" {{action "addRestaurant" restaurant}}></i>'
+		return Ember.String.htmlSafe('<i class="fa fa-star-0 fa-2x" {{action "addRestaurant" restaurant}}></i>');
 	}
 }
 
